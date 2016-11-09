f6c272bd2bd194dead5b2a50a51174b0ad28e3e8
Jujuxw <13389634266.189.cn>
2016-11-09 16:07:09 +0800

    Initial Commit

---
+++ SQlite_Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQlite_Study
+//
+//  Created by juju on 2016/11/9.
+//  Copyright © 2016年 juju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
